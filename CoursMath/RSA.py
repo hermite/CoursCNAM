f7a19985efc4896b6359456c4f268c16dc943554
hermite <3yx3zmot>
2021-12-01 19:16:29 +0100

    execice math fini

--- CoursMath/RSA.py
+++ CoursMath/RSA.py
@@ -52,23 +52,45 @@ tabCODAGE = [["A", 1], ["B", 2], ["C", 3], ["D", 4], ["E", 5], ["F", 6], ["G", 7
 p=5
 q=11
 n=p*q
+m=(p-1)*(q-1)
 c=9
 d=9
+print("----------------------------------")
+print ("test des algorythmes sur les valeurs de l'exercice :")
+print("----------------------------------")
 print(cryptChaine("IREMAUVERGNE",tabCODAGE,n,c))
 print(decryptChaine("48|20|29|5|12|18|20|21|8|20|14|20|12|1|31|20|8|25|",tabCODAGE,n,d))
 
 
-#table de codage
+#Autre test
+
+#calcul la valeur de d
+def CalculD(m,c):
+    i=1
+    while (c*i)%m!=1:
+        i = i + 1
+    return i
+
 p2=13
 q2=19
 n2=p*q
+m2=(p-1)*(q-1)
 c2=23
-d2=9
+d2=CalculD(m2,c2)
+
+#Table de codage 2
 tabCODAGE2 = [["A", 26], ["B", 39], ["C", 16], ["D", 29], ["E", 42], ["F", 6], ["G", 20], ["H", 33], ["I", 46], ["J", 23], ["K", 11], ["L", 37], ["M", 25],
              ["N", 38], ["O", 27], ["P", 40], ["Q", 41], ["R", 30], ["S", 19], ["T", 44], ["U", 45], ["V", 9], ["W", 47], ["X", 24], ["Y", 12],
              ["Z", 26], ["a", 15], ["b", 3], ["c", 17], ["d", 5], ["e", 31], ["f", 7], ["g", 8], ["h", 22], ["i", 10], ["j", 36], ["k", 49],
              ["l", 50], ["m", 51], ["n", 52], ["o", 4], ["p", 18], ["q", 43], ["r", 32], ["s", 21], ["t", 34], ["u", 35], ["v", 48], ["w", 13],
              ["x", 1], ["y", 2], ["z", 28]]
 #Autre test
-print(cryptChaine("Ce fut un exercice interessant",tabCODAGE2,55,9))
-print(decryptChaine("48|16| |32|37|6| |37|30| |16|35|16|44|19|50|19|16| |50|30|6|16|44|16|45|45|42|30|6|",tabCODAGE,55,9))
+print ("----------------------------------------------")
+print()
+print()
+print ("----------------------------------------------")
+print("test de l'algorythmes avec un autre tableau de codage et de nouvelles clés :")
+print("-----------------------------------------------")
+print("la premier valeur de d trouvée est : " + str(d2))
+print("Premier cryptage : " + cryptChaine("Chiffrement RSA",tabCODAGE2,n2,c2))
+print("Décryptage d'un message : " + decryptChaine("26|36| |13|30|34| |30|28| |36|1|36|43|18|10|18|36| |10|28|34|36|43|36|21|21|20|28|34|",tabCODAGE2,n2,d2))
